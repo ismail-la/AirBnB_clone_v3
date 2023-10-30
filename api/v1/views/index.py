b8ef4fe3711101e0d9077c5724b264c7400a34a7
ismail-la <lahbariismail@gmail.com>
2023-10-30 20:18:23 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,9 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models.amenity import Amenity
-from models.city import City
+from models.city import Cit
+from models.place import Placey
 from models.review import Review
-from models.place import Place
 from models.state import State
 from models.user import User
 
