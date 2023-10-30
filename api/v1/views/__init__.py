c508601647a1d1ef3e085b212f1977d8f2cd1313
ismail-la <lahbariismail@gmail.com>
2023-10-30 14:15:40 +0100

    task3

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that sets up a blueprint for a Flask application.
+-initializing app_views
+"""
+
+# Blueprints in Flask are used for modular structuring of applications.
+from flask import Blueprint
+
+# Creates a new blueprint named app_views.
+# The url_prefix='/api/v1' means that all routes registered with
+#  this blueprint will have URLs prefixed with /api/v1.
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
+"""
+These modules are likely to contain route handlers for different parts of your
+application (like index, states, cities, amenities, users, places,
+places_reviews, and places_amenities).
+"""
---
+++ api/v1/views/index.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that sets up routes for the Flask application
+"""
+# Imports the app_views object from the api.v1.views module.It’s likely that
+#   app_views is a blueprint that contains routes for your application.
+from api.v1.views import app_views
+
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    This function mapped to the route /status and will return a JSON response
+    with the status ‘OK’ when accessed with a GET request.
+    """
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    This function is mapped to the route /stats and will return a JSON response
+    with the count of each object type when accessed with a GET request.
+    The counts are obtained by calling the count() method on the storage object
+    for each class.
+    """
+    from models import storage
+    return jsonify({'amenities': storage.count(Amenity),
+                    'cities': storage.count(City),
+                    'reviews': storage.count(Review),
+                    'places': storage.count(Place),
+                    'states': storage.count(State),
+                    'users': storage.count(User)})
