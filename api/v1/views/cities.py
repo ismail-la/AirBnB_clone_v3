cf105470da3327b20b3cda6b938db0ea7a4da706
ismail-la <lahbariismail@gmail.com>
2023-10-30 20:43:06 +0100

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,73 +1,66 @@
 #!/usr/bin/python3
+"""Script that controls API functions for city objects"""
 
 
+# imports necessary modules and objects
 from api.v1.views import app_views
-from models import storage
+from flask import abort, request, jsonify
 from models.state import State
 from models.city import City
-from flask import abort, request, jsonify
+from models import storage
 
 
-@app_views.route("/states/<state_id>/cities", strict_slashes=False,
-                 methods=["GET"])
-def cities(state_id):
+# This route handles two types of HTTP requests: POST and GET
+@app_views.route('/states/<state_id>/cities', methods=['POST', 'GET'],
+                 strict_slashes=False)
+def get_city(state_id):
 
-    cities_list = []
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    cities = state.cities
-    for city in cities:
-        cities_list.append(city.to_dict())
-    return jsonify(cities_list)
+    if request.method == 'POST':
+        information = request.get_json(silent=True)
+        if not information:
+            abort(400, 'Not a JSON')
+        if 'name' not in information.keys():
+            abort(400, 'Missing name')
+        information['state_id'] = state_id
+        new_city = City(**information)
+        new_city.save()
+        return jsonify(new_city.to_dict()), 201
+    if request.method == 'GET':
+        city_list = []
+        for city in state.cities:
+            city_list.append(city.to_dict())
+        return jsonify(city_list)
 
 
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
-def cities_id(city_id):
+# This route handles three types of HTTP requests: DELETE, GET and POST
+@app_views.route('/cities/<city_id>', methods=['DELETE', 'GET', 'PUT'],
+                 strict_slashes=False)
+def city_methods(city_id):
 
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    return jsonify(city.to_dict())
-
-
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["DELETE"])
-def city_delete(city_id):
-
-    obj = storage.get(City, city_id)
-    if obj is None:
-        abort(404)
-    storage.delete(obj)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route("/states/<state_id>/cities", strict_slashes=False,
-                 methods=["POST"])
-def create_city(state_id):
-    """create a new post req"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    if "name" not in data:
-        abort(400, "Missing name")
-    new_state = City(state_id=state.id, **data)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
-
-
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["PUT"])
-def update_city(city_id):
-    """update city"""
-    obj = storage.get(City, city_id)
-    if obj is None:
-        abort(404)
-    data = request.get_json(force=True, silent=True)
-    if not data:
-        abort(400, "Not a JSON")
-    obj.name = data.get("name", obj.name)
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    if request.method == 'DELETE':
+        for place in city.places:
+            for review in place.reviews:
+                storage.delete(review)
+            storage.delete(place)
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    if request.method == 'GET':
+        return jsonify(city.to_dict())
+    if request.method == 'PUT':
+        information = request.get_json()
+        if not information:
+            abort(400, 'Not a JSON')
+        for key, value in information.items():
+            if key in ['id', 'state_id', 'created_at', 'updated_at']:
+                pass
+            else:
+                setattr(city, key, value)
+        city.save()
+        return (city.to_dict()), 200
