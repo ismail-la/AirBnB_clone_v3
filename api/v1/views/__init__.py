6c1392a74bc6ef5cfa692c56914efb5b53de7f77
ismail-la <lahbariismail@gmail.com>
2023-10-31 02:12:50 +0100

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -18,8 +18,8 @@ places_reviews, and places_amenities).
 """
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
